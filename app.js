2450724820c0b31844a04f911d9fce9341bf8e52
Daniel John <danieljohncode@tutanota.com>
2020-09-27 12:21:19 +0100

    Added the option for user input

--- app.js
+++ app.js
@@ -1,3 +1,24 @@
 console.log("Built in 2020 by danieljcode");
 console.log("https://github.com/danieljcode");
 
+const inputMain = document.getElementById("mainSelector");
+
+var countryCode = "GB";
+
+var requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+  };
+  
+
+fetch(`https://covid19-api.org/api/status/${countryCode}`, requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
+    .catch(error => console.log('error', error));
+
+inputMain.addEventListener("change", () =>{
+
+    console.log(inputMain.value);
+    countryCode = inputMain.value;
+
+})
